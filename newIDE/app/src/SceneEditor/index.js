fc2cadff712a1ad79b39fc897f00b963c2b0d833
Davy Hélard <davy.helard@gmail.com>
2025-02-14 18:15:13 +0100

    Allow to edit variants.

--- newIDE/app/src/SceneEditor/index.js
+++ newIDE/app/src/SceneEditor/index.js
@@ -101,6 +101,7 @@ type Props = {|
   layout: gdLayout | null,
   eventsFunctionsExtension: gdEventsFunctionsExtension | null,
   eventsBasedObject: gdEventsBasedObject | null,
+  eventsBasedObjectVariant: gdEventsBasedObjectVariant | null,
 
   globalObjectsContainer: gdObjectsContainer | null,
   objectsContainer: gdObjectsContainer,
@@ -126,7 +127,8 @@ type Props = {|
   ) => void,
   onOpenEventBasedObjectEditor: (
     extensionName: string,
-    eventsBasedObjectName: string
+    eventsBasedObjectName: string,
+    variantName: string
   ) => void,
 
   // Preview:
@@ -1556,7 +1558,9 @@ export default class SceneEditor extends React.Component<Props, State> {
                   ),
                   gd.PlatformExtension.getObjectNameFromFullObjectType(
                     object.getType()
-                  )
+                  ),
+                  // TODO open the right variant
+                  ''
                 ),
             }
           : null,
@@ -1887,6 +1891,7 @@ export default class SceneEditor extends React.Component<Props, State> {
       layout,
       eventsFunctionsExtension,
       eventsBasedObject,
+      eventsBasedObjectVariant,
       initialInstances,
       resourceManagementProps,
       isActive,
@@ -1952,6 +1957,7 @@ export default class SceneEditor extends React.Component<Props, State> {
                 layout={layout}
                 eventsFunctionsExtension={eventsFunctionsExtension}
                 eventsBasedObject={eventsBasedObject}
+                eventsBasedObjectVariant={eventsBasedObjectVariant}
                 layersContainer={this.props.layersContainer}
                 globalObjectsContainer={this.props.globalObjectsContainer}
                 objectsContainer={this.props.objectsContainer}
@@ -2113,6 +2119,9 @@ export default class SceneEditor extends React.Component<Props, State> {
                           this.updateBehaviorsSharedData()
                         }
                         openBehaviorEvents={this.props.openBehaviorEvents}
+                        onOpenEventBasedObjectEditor={
+                          this.props.onOpenEventBasedObjectEditor
+                        }
                       />
                     )}
                   </React.Fragment>
@@ -2238,22 +2247,25 @@ export default class SceneEditor extends React.Component<Props, State> {
                   resourceManagementProps={this.props.resourceManagementProps}
                 />
               )}
-              {this.state.scenePropertiesDialogOpen && eventsBasedObject && (
-                <EventsBasedObjectScenePropertiesDialog
-                  project={project}
-                  eventsBasedObject={eventsBasedObject}
-                  onClose={() => this.openSceneProperties(false)}
-                  onApply={() => this.openSceneProperties(false)}
-                  getContentAABB={
-                    this.editorDisplay
-                      ? this.editorDisplay.instancesHandlers.getContentAABB
-                      : () => null
-                  }
-                  onEventsBasedObjectChildrenEdited={
-                    this.props.onEventsBasedObjectChildrenEdited
-                  }
-                />
-              )}
+              {this.state.scenePropertiesDialogOpen &&
+                eventsBasedObject &&
+                eventsBasedObjectVariant && (
+                  <EventsBasedObjectScenePropertiesDialog
+                    project={project}
+                    eventsBasedObject={eventsBasedObject}
+                    eventsBasedObjectVariant={eventsBasedObjectVariant}
+                    onClose={() => this.openSceneProperties(false)}
+                    onApply={() => this.openSceneProperties(false)}
+                    getContentAABB={
+                      this.editorDisplay
+                        ? this.editorDisplay.instancesHandlers.getContentAABB
+                        : () => null
+                    }
+                    onEventsBasedObjectChildrenEdited={
+                      this.props.onEventsBasedObjectChildrenEdited
+                    }
+                  />
+                )}
               {!!this.state.layoutVariablesDialogOpen && layout && (
                 <SceneVariablesDialog
                   open
